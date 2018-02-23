055432adac54175c6f4575ad74b05199c2867090
semantic <semantic@robinson.eurecom.fr>
2018-02-23 15:08:41 +0100

    add test model

--- src/word2vec.py
+++ src/word2vec.py
@@ -1,5 +1,4 @@
 from gensim.models import Word2Vec
-from gensim.models.word2vec import LineSentence
 import argparse
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -7,8 +6,9 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 parser = argparse.ArgumentParser(description="Run word2vec")
 
-parser.add_argument('--file')
+parser.add_argument('--file', default=None, required=True)
 parser.add_argument('--workers', type=int, default=4)
+parser.add_argument('--test', default=False, action='store_true')
 
 args = parser.parse_args()
 
@@ -30,12 +30,37 @@ class MySentences(object):
 
             yield line
 
-sentences = MySentences(args.file)
+if not args.test:  # train the model
 
-model = Word2Vec(sentences, workers=args.workers, min_count=0)
+    sentences = MySentences(args.file)
 
-file_name = args.file.split('/')[1]
-file_name = file_name.split('.')[0]
+    model = Word2Vec(sentences, workers=args.workers, min_count=0)
 
-model.save('models/'+file_name+'.wv')
+    file_name = args.file.split('/')[1]
+    file_name = file_name.split('.')[0]
 
+    model.save('models/'+file_name+'wv')
+
+else:  # test the model
+
+    model = Word2Vec.load(args.file)
+
+    word_vectors = model.wv
+
+    del model  # save memory
+
+    key = 'spotify:track:2PpruBYCo4H7WOBJ7Q2EwM'  # OutKast, Hey Ya
+
+    print(word_vectors.most_similar(key))
+
+    # Results:
+    # OutKast, Ms. Jackson
+    # OutKast, The Way You Move - Club Mix
+    # OutKast, Roses
+    # R. Kelly, Ignition - Remix
+    # OutKast, So Fresh, So Clean
+    # Usher, Yeah!
+    # Beyoncé, Crazy In Love
+    # Shakira, Hips Don't Lie
+    # Gnarls Barkley, Crazy
+    # Kanye West, Gold Digger
